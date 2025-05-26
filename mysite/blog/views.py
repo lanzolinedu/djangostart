d96c50dda6559683549a8bc537a578fa04cf3b7d
Luca Anzolin Edu <luca.anzolin.edu@gmail.com>
2025-05-26 10:50:39 +0200

    form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
+
+    testo = request.POST.get('testo')
+    numero = request.POST.get('numero')
+
     context = {
-        'messaggio': 'Benvenuto nella homepage!'
+        'messaggio': 'Benvenuto nella homepage!',
+        'testo':testo,
+        'numero':numero
     }
 
     return render(request, 'home.html', context)
